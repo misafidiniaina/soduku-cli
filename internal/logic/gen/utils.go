75da455fb2b1664d0f6f0734e4ec3cd792a1e33d
misafidiniaina <liantsoatsiorinirina@gmail.com>
2026-06-18 21:13:58 +0300

    chore(refactore): refactor the missingvalue function

--- internal/logic/gen/utils.go
+++ internal/logic/gen/utils.go
@@ -1,11 +1,13 @@
 package gen
 
-func PossibleValue(group [9]int) []int{
+import "slices"
+
+func MissingValue(group [9]int) []int{
 	var result []int
 	normal := []int{1,2,3,4,5,6,7,8,9}
 
 	var PresentValues []int
-	for i := 0; i < len(group); i++ {
+	for i := range len(group) {
 		if group[i] != 0 {
 			PresentValues = append(PresentValues, group[i])
 		}
@@ -13,13 +15,7 @@ func PossibleValue(group [9]int) []int{
 
 	// build result as values from normal that are not present
 	for _, v := range normal {
-		skip := false
-		for _, pv := range PresentValues {
-			if v == pv {
-				skip = true
-				break
-			}
-		}
+		skip := slices.Contains(PresentValues, v)
 		if !skip {
 			result = append(result, v)
 		}
